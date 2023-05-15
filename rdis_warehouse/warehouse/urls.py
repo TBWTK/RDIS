a0a22794ba5c9897af53f25019180d9eed256fa1
TBWTK <ilyapim18@gmail.com>
2023-05-15 05:24:03 +0300

    add export excel

--- rdis_warehouse/warehouse/urls.py
+++ rdis_warehouse/warehouse/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import logout_view, login_view, main_view, update_view
+from .views import logout_view, login_view, main_view, export_excel
 from django.contrib import admin
 
 
@@ -8,7 +8,7 @@ app_name = 'school'
 urlpatterns = [
     path('', main_view, name='main'),
     path('/admin/storage/<int:pk>/change/', admin.site.urls, name='update'),
-    path('/<int:value>/$', update_view, name='update'),
+    path('export/', export_excel, name='export_excel'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
 ]
--- rdis_warehouse/warehouse/views.py
+++ rdis_warehouse/warehouse/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Storage, InventoryType, StorageLocation
+from openpyxl import Workbook
+from openpyxl.styles import NamedStyle, Font, Alignment
+from openpyxl.styles.numbers import NumberFormat
 
 
 def main_view(request):
@@ -10,19 +14,59 @@ def main_view(request):
     return render(request, 'table.html', {'stuff': stuff})
 
 
-def update_view(request, value):
-    item = Storage.objects.get(id_storage=value)
-    type_inventory = InventoryType.objects.filter(user=request.user.id)
-    locations = StorageLocation.objects.all()
-    return render(request, 'update.html', {'item': item,  'type_inventory': type_inventory, 'locations': locations})
+def export_excel(request):
+    if request.method == 'POST':
+        # Получаем количество записей, которые нужно экспортировать
+        quantity = int(request.POST.get('quantity'))
 
+        # Получаем записи из базы данных
+        records = Storage.objects.filter(inventory__type__user=request.user.id,
+                                         inventory__is_active=True).order_by('-id_storage')[:quantity]
 
-def search_view(request):
-    pass
+        # Создаем новый Excel-файл и добавляем в него записи
+        wb = Workbook()
+        ws = wb.active
 
+        ws.append(["Расположение", "Тип инвентаря",  "Название", "Инвентарный номер",
+                   "Характеристики", "Закрепленный сотрудник", "Дата начала", "Дата окончания",
+                   "Комментарий"])
 
-def add_view(request):
-    pass
+        ws.column_dimensions['A'].width = 30
+        ws.column_dimensions['B'].width = 30
+        ws.column_dimensions['C'].width = 30
+        ws.column_dimensions['D'].width = 30
+        ws.column_dimensions['E'].width = 30
+        ws.column_dimensions['F'].width = 30
+        ws.column_dimensions['G'].width = 30
+        ws.column_dimensions['H'].width = 30
+        ws.column_dimensions['I'].width = 30
+
+        # Устанавливаем формат ячейки для заголовков
+        header_font = Font(name='Times New Roman', size=14, bold=True)
+        header_alignment = Alignment(horizontal='center', vertical='center')
+        for cell in ws[1]:
+            cell.font = header_font
+            cell.alignment = header_alignment
+
+        for record in records:
+            ws.append([record.location.location_name, record.inventory.type.type_name,
+                       record.inventory.inventory_name, record.inventory.inventory_number,
+                       record.inventory.specifications,
+                       record.worker.first_name + " " + record.worker.name,
+                       record.start_date, record.end_date, record.comment])
+
+        #date_style = NamedStyle(name='date_style')
+        #date_style.number_format = NumberFormat('dd.mm.yyyy')
+
+        #for row in ws.iter_rows(min_row=2, max_row=len(records)+1, min_col=8, max_col=9):
+        #    for cell in row:
+        #        cell.style = date_style
+
+        # Возвращаем Excel-файл в ответе на запрос
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=export.xlsx'
+        wb.save(response)
+        return response
 
 
 def my_view(request, pk):
